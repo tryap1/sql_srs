a9f193073125d16c6b9f5ef56d285146c66e2809
Tim Yap <timyap@tims-macbook-pro-1.home>
2023-11-28 21:37:56 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import duckdb
+
+st.write("Hello World :D")
+data = {"a" : [1, 2, 3], "b" : [4, 5, 6]}
+df = pd.DataFrame(data)
+
+tab1, tab2, tab3, = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1 :
+    st.header('A cat')
+    st.image("https://static.streamlit.io/examples/cat.jpg", width = 200)
+
+    sql_query = st.text_area(label = "Insert your sql query")
+    result = duckdb.query(sql_query)
+
+    st.write("Inserted Request : {}".format(sql_query))
+    st.dataframe(result.df())
+
+with tab2 :
+    st.header('A Dog')
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+with tab3 :
+    st.header(' An owl')
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
